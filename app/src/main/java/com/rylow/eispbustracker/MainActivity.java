6761577ff402d357e5a3e3bd0d5d03f2213248f6
Rylow <bakhtin.s@gmail.com>
2016-02-07 20:02:06 +0100

    crypto + osm integration

--- app/src/main/java/com/rylow/eispbustracker/MainActivity.java
+++ app/src/main/java/com/rylow/eispbustracker/MainActivity.java
@@ -1,22 +1,41 @@
 package com.rylow.eispbustracker;
 
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
 import com.rylow.eispbustracker.network.Connect;
 
 public class MainActivity extends AppCompatActivity {
+
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+    private TextView tView1;
+
+    private Connect connect = new Connect();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        tView1 = (TextView) findViewById(R.id.txtView1);
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -26,10 +45,69 @@ public class MainActivity extends AppCompatActivity {
                 //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 //        .setAction("Action", null).show();
 
-                Thread connectThread = new Thread(new Connect());
+                configureLocation();
+                Thread connectThread = new Thread(connect);
                 connectThread.start();
             }
         });
+
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+
+                connect.sendLocationData(String.valueOf(location.getLatitude()), String.valueOf(location.getLongitude()));
+
+
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(intent);
+            }
+        };
+
+
+
+
+
+
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults){
+        switch (requestCode){
+
+            case (10):
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                    configureLocation();
+                return;
+        }
+
+    }
+
+    private void configureLocation() {
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.INTERNET}, 5);
+            }
+            return;
+        }
+
+        locationManager.requestLocationUpdates("gps", 5000, 20, locationListener);
     }
 
     @Override
--- app/src/main/java/com/rylow/eispbustracker/network/Connect.java
+++ app/src/main/java/com/rylow/eispbustracker/network/Connect.java
@@ -5,19 +5,26 @@ import android.util.Log;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.security.GeneralSecurityException;
-
-
+import java.security.InvalidKeyException;
 
 
 public class Connect implements Runnable {
 
     Socket clientSocket = new Socket();
+
+    BufferedReader inFromServer;
+    BufferedWriter os;
+
+    String sessionKey;
 
     @Override
     public void run(){
@@ -30,14 +37,19 @@ public class Connect implements Runnable {
             clientSocket.connect(new InetSocketAddress("192.168.1.37", 6789), 1000);
 
             JSONObject json = new JSONObject();
-            json.put("type", "CONNECT");
+            json.put("check", "ok");
 
-            //OutputStreamWriter os = new OutputStreamWriter(clientSocket.getOutputStream());
-            BufferedWriter os = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+            this.inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            this.os = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+
+
             try {
-                Log.v("aa",json.toString());
-                os.write(TwoFish.encrypt(json.toString(), "VeryC0oLVeryC0oL"));
+                this.sessionKey = TwoFish.decrypt(inFromServer.readLine(), "VeryC0oLVeryC0oL").trim();
+
+                Log.v("aa",sessionKey);
+                os.write(TwoFish.encrypt(json.toString(), sessionKey));
                 os.newLine();
+                os.flush();
 
             } catch (GeneralSecurityException e) {
                 e.printStackTrace();
@@ -45,9 +57,47 @@ public class Connect implements Runnable {
 
 
 
-            os.close();
+            //os.close();
         }
         catch(IOException|JSONException e){
+            e.printStackTrace();
+        }
+
+
+    }
+
+    public void sendLocationData(String locationX, String locationY){
+
+        JSONObject outLocation = new JSONObject();
+
+        try {
+            outLocation.put("packetType", 101);
+            outLocation.put("locationX", locationX);
+            outLocation.put("locationY", locationY);
+
+            Log.v("aa", locationX);
+
+            Log.v("aa", outLocation.toString());
+
+
+
+            String s = TwoFish.encrypt(outLocation.toString(), sessionKey);
+
+            Log.v("aa", String.valueOf(s.length()));
+
+            Log.v("aa", s);
+
+            os.write(s);
+            os.newLine();
+            os.flush();
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (InvalidKeyException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
             e.printStackTrace();
         }
 
